7470e201a7f29a46ee28dd33621026e5dd38e941
Stella <feelis1119@naver.com>
2020-03-04 18:55:01 -0500

    addingbasic

--- index.php
+++ index.php
@@ -9,9 +9,7 @@
 </head>
 <body>
     <h2>Hi Devs! I generate a tag to "real" Home page for your convience!</h2>
-    
-    <a href="index.html">Click to Real Home!!</a>
-
-    <p>Don't panic when you see this bashboard page without any style!!</p>
+    <a href="index.html">Home!!</a>
+    <p>Don't panic when you see this dashboard page without any style!!</p>
 </body>
 </html>
