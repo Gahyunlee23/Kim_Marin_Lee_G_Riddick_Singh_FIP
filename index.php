5e1c24283832932c8d34f4469a2ca43e5758e088
Stella <feelis1119@naver.com>
2020-03-04 18:52:07 -0500

    adding

--- index.php
+++ index.php
@@ -3,8 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/main.css">
     <title>Dashboard</title>
 </head>
 <body>
+    <h2>Hi Devs! I generate a tag to "real" Home page for your convience!</h2>
+    
+    <a href="index.html">Click to Real Home!!</a>
+
+    <p>Don't panic when you see this bashboard page without any style!!</p>
 </body>
 </html>
