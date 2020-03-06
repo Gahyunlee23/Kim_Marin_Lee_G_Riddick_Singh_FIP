247135938046b2d41f137409acabcf70154cdc8f
Stella <feelis1119@naver.com>
2020-03-06 16:57:04 -0500

    copies, adding db ;
    dk;mfskdf

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ class Database
     // Note: specify your own database credentials
     private $host = "localhost";
 
-    private $db_name = "db_fip";
+    private $db_name = "db_";
 
     private $username = "root";
 
