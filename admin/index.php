ba5758e5c5286fad06499daec27b6139989fa8cb
Dana Marin <d_maringarcia@fanshaweonline.ca>
2020-04-12 23:19:13 -0400

    Adding login instructions txt file + readme

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,7 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <h2>Welcome! <?php echo $_SESSION['login_fname']; ?></h2>
+    <h2>Welcome, <?php echo $_SESSION['login_fname']; ?></h2>
 
     <a href="admin_logout.php">Sign Out</a>
     <a href="admin_create_user.php">Create User</a>
